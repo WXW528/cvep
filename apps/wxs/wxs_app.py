f2a9efa4dcbba50c01c95053fb31c70cc58aeb11
yantao <yt7589@qq.com>
2020-08-23 20:28:01 +0800

    fix BUG20200823006

--- apps/wxs/wxs_app.py
+++ apps/wxs/wxs_app.py
@@ -9,7 +9,7 @@ class WxsApp(object):
     def startup(self, args={}):
         print('无锡所应用...')
         MMongoDb._initialize()
-        run_mode = 1
+        run_mode = 2
         if 1 == run_mode:
             #WxsDsm.generate_txt_by_wxs_tds_ok_images()
             WxsDsm.generate_ds_from_wxs_tds_ibc()
