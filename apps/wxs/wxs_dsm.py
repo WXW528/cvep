6d98c4fbfb3d9d31d106d0ff3e21551242622199
yantao <yt7589@qq.com>
2020-08-23 12:54:34 +0800

    get image file and corresponding org_bmy_id

--- apps/wxs/wxs_dsm.py
+++ apps/wxs/wxs_dsm.py
@@ -13,18 +13,17 @@ class WxsDsm(object):
         从无锡所图片文件和对应的年款编中，求出对应的简化年款编号和品牌索引号
         '''
         bmy_code_bmy_id_dict = CBmy.get_bmy_code_bmy_id_dict()
-        for k, v in bmy_code_bmy_id_dict.items():
-            print('### {0}: {1};'.format(k, v))
-        i_debug = 1
-        if 1 == i_debug:
-            return 
         with open('./work/wt_img_bmy_code.txt', 'r', encoding='utf-8') as fd:
             for line in fd:
                 line = line.strip()
                 arrs_a = line.split('*')
                 img_file = arrs_a[0]
                 bmy_code = arrs_a[1]
-                print('{0}: {1};'.format(img_file, bmy_code))
+                if bmy_code in bmy_code_bmy_id_dict:
+                    org_bmy_id = bmy_code_bmy_id_dict[bmy_code] - 1
+                    print('##: {0}*{1};'.format(img_file, org_bmy_id))
+                else:
+                    print('@@@@@@@@@@@@@@@@@@@@@@@@ {0};'.format(bmy_code))
 
     @staticmethod
     def generate_txt_by_wxs_tds_ok_images():
