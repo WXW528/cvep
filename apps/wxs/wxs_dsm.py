7cab98b97866cdf769a8cc9d073e3cb4ca42499f
yantao <yt7589@qq.com>
2020-08-23 12:22:45 +0800

    add image file and bmy code to text file

--- apps/wxs/wxs_dsm.py
+++ apps/wxs/wxs_dsm.py
@@ -11,11 +11,12 @@ class WxsDsm(object):
         将无锡所测试集中标注出年款的图片文件名和年款编号写进文件文件中
         '''
         base_path = Path('E:/work/tcv/temp/wlok')
-        for img_obj in base_path.iterdir():
-            full_fn = str(img_obj)
-            arrs_a = full_fn.split('-')
-            prefix = arrs_a[0]
-            arrs_b = arrs_a[0].split('\\')
-            bmy_code = arrs_b[-1]
-            img_file = full_fn[len(prefix)+1:]
-            print('{0}: {1};'.format(bmy_code, img_file))
+        with open('./work/wt_img_bmy_code.txt', 'w+', encoding='utf-8') as wfd:
+            for img_obj in base_path.iterdir():
+                full_fn = str(img_obj)
+                arrs_a = full_fn.split('-')
+                prefix = arrs_a[0]
+                arrs_b = arrs_a[0].split('\\')
+                bmy_code = arrs_b[-1]
+                img_file = full_fn[len(prefix)+1:]
+                wfd.write('{0}*{1}\n'.format(img_file, bmy_code))
