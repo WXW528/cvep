d59d865f95af090061ffe5104fe561a3000b0a72
yantao <yt7589@qq.com>
2020-08-23 12:46:21 +0800

    initialize mongodb at the beginning of the app

--- app_main.py
+++ app_main.py
@@ -1,7 +1,9 @@
 #
+from apps.wxs.model.m_mongodb import MMongoDb
 from apps.wxs.wxs_app import WxsApp
 
 def main(args={}):
+    MMongoDb._initialize()
     app = WxsApp()
     params = {}
     app.startup(params)
